c85f4e4644d28b5fa7a915cc3b3bbced7c500aa2
Jeff Casimir <jeff@casimircreative.com>
2011-03-03 20:35:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Assasin::Application.config.secret_token = '48671c5d83ba7589b830fb412d3217d15f3ccbb3bd33ca1f45dc7d43c795ca299a89b70865eb40636d5a8398c64b72960ad83dcd408f85de3def2aa9e15e7d2f'
